4a4da360ca855d294f99b49c8f51f05a8c7ff14f
juniorchavesv <chavesjuniorvasconcelos@gmail.com>
2025-07-25 15:56:46 -0300

    Service layer, component registration

---
+++ src/main/java/com/vasconcelosjuniorweb/curso/services/UserService.java
@@ -0,0 +1,31 @@
+package com.vasconcelosjuniorweb.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vasconcelosjuniorweb.curso.entities.User;
+import com.vasconcelosjuniorweb.curso.repositories.UserRepository;
+
+
+@Service  // REGISTRADO COMO COMPONENTE
+public class UserService {
+	
+	@Autowired // FAZ A INJEÇÃO DE DEPENDENCIA DE FORMA TRANSPARENTE
+	private UserRepository repository;
+	
+	//MÉTODO RETORNA TODOS OS USUÁRIOS DO BANCO
+	public List<User> findAll(){
+		return repository.findAll();
+		
+	}
+	
+	//MÉTODO RETORNA TODOS OS USUÁRIOS DO BANCO PELO ID
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+		
+	}
+}
